eeffbd22130c279ea4f5bffe0c0a6a86028fa2ab
Walter Cegarra <walter.cegarra@icloud.com>
2019-07-25 21:42:13 +0100

    simple airbnb

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,6 +1,11 @@
 class FlatsController < ApplicationController
+  before_action :set_flat, only: [:show]
+
   def index
-    @flats = Flat.all
+    @flats = Flat.all.reverse
+  end
+
+  def show
   end
 
   def new
@@ -20,5 +25,9 @@ class FlatsController < ApplicationController
 
   def flat_params
     params.require(:flat).permit(:name, :address, :description, :price_per_night, :number_of_guests, :picture)
+  end
+
+  def set_flat
+    @flat = Flat.find(params[:id])
   end
 end
